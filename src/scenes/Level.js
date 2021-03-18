c874619b3059efe6a011d506e90d85fd281de817
CarlusTho <33193462+CarlusTho@users.noreply.github.com>
2021-03-18 15:56:46 +0100

    Fonction cliquer augmente le score

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -1,17 +1,21 @@
 import Phaser from '../lib/phaser.js';
+//import { GameObjects } from 'phaser';
+
 
 export default class Level extends Phaser.Scene
 {
-    score = 500
+
+    score;
 
     constructor()
     {
         super('level')
+        
     }
 
     init()
     {
-        
+        this.score = 500;
     }
 
     preload()
@@ -19,36 +23,45 @@ export default class Level extends Phaser.Scene
         //  Preload des images
         this.load.image('space1', 'assets/background/purple.png')
         this.load.image('mine', 'assets/sprite/meteor_squareDetailedLarge.png')
+
+        //  Preload des contrôles (Souris)
+        this.pointer = this.input.activePointer;
+    
+
     }
 
     create()
     {
 
         //  Pose des images
-        this.add.image(128, 128, 'space1').setScale(2)
-        this.add.image(256, 556, 'mine')
+        this.add.image(128, 128, 'space1').setScale(2);
+        this.mine = this.physics.add.image(256, 556, 'mine');
 
 
         //  Définition du score
-        const styleScore = {fontSize: 20}
-        this.scoreText = this.add.text(10, -120, 'Score: 0', styleScore)
+        const styleScore = {fontSize: 20};
+        this.scoreText = this.add.text(10, -120, `Score: ${this.score}`, styleScore);
 
 
+    }
+
+    collectScore()
+    {
+        this.score++;
+        const value = `Score: ${this.score}`;
+        this.scoreText.text = value;
     }
 
     update()
     {
-
+        let truc = this;
+        this.mine.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
+            truc.collectScore()
+        });
+        
     }
 
     
-
-    collectScore()
-    {
-        this.score++
-        const value = `Score: ${this.score}`
-        this.scoreText.text = value
-    }
 
 
 
