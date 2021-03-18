59b28cd11c080344de71d9da6223ee2ebb7a563f
CarlusTho <33193462+CarlusTho@users.noreply.github.com>
2021-03-18 12:24:01 +0100

    Ajout de sprites à l'écran

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -12,9 +12,16 @@ export default class Level extends Phaser.Scene
         
     }
 
+    preload()
+    {
+        this.load.image('space1', 'assets/background/purple.png')
+        this.load.image('mine', 'assets/sprite/meteor_squareDetailedLarge.png')
+    }
+
     create()
     {
-        
+        this.add.image(128, 128, 'space1').setScale(2)
+        this.add.image(256, 556, 'mine')
     }
 
     update()
