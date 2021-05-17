dcd295996685cd35912e234a86a879fc4c04f554
Raphaël Masse <masseraphael76@gmail.com>
2021-05-17 20:30:23 +0200

    structuration du level

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -12,6 +12,21 @@ export default class Level extends Phaser.Scene
         
     }
 
+    preload()
+    {
+        //  Preload des images
+        this.load.image('space1', 'assets/background/purple.png')
+        this.load.image('mine1', 'assets/sprite/meteor_squareDetailedLarge.png')
+        this.load.image('enemy1', 'assets/sprite/enemy_A.png')
+
+        //  Preload des contrôles (Souris)
+        this.pointer = this.input.activePointer;
+    
+
+    }
+
+    //----------------------------------------------------CREATE--------------------------------------------------------------//
+
     create()
     {
         
